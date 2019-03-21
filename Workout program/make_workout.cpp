611860cadf5b25b126a9e0ed65261dc382f485d1
brambovich <brammeijer12@gmail.com>
2019-03-21 10:36:52 +0100

    tussendoor

--- Workout program/make_workout.cpp
+++ Workout program/make_workout.cpp
@@ -5,21 +5,26 @@ using namespace std;
 
 int main () {
   string answer_create_workout = "yes";
-  string answer = "yes";
+  string answer;
   string linetext;
 
 while (answer_create_workout == "yes"){
 	cout << "welcome you are now making a workout program. \n";
 	cout << "how would you name this workout program?\n";
-	string name;
+	string name, txtname;
 	std::getline (std::cin,name);
-	name = name + ".txt";
-	ofstream myfile ("example.txt");
+	txtname = name + ".txt";
+	ofstream myfile (txtname);
 	myfile << "name: " << name <<"\n";
 	cout << "wourkout created called:" << name << "now we will get all data...";
+	
+	cout << "Would you like to create a new program? \n";
+	getline (cin,answer_create_workout);
+	
+	
   
 }
-  else cout << "Unable to open file";
+
   return 0;
 }
 
