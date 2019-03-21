fcc183c75f0fd23a678ca12c7d5de54dd135da4a
brambovich <brammeijer12@gmail.com>
2019-03-21 13:38:51 +0100

    code writing workout program

--- Workout program/make_workout.cpp
+++ Workout program/make_workout.cpp
@@ -1,19 +1,20 @@
 // writing on a text file
 #include <iostream>
 #include <fstream>
+#include <stdlib.h>
 using namespace std;
 
 int main () {
   string answer_create_workout = "yes";
   string answer;
   string linetext;
-
+  
 while (answer_create_workout == "yes"){
 	
 	cout << "welcome you are now making a workout program. \n";
 	cout << "how would you name this workout program?\n";
 	string name, txtname;
-	getline (cin,name);
+	cin >> name;
 	txtname = name + ".txt";
 	ofstream myfile (txtname);
 	myfile << "name: " << name <<"\n";
@@ -21,20 +22,33 @@ while (answer_create_workout == "yes"){
 	
 
 	cout << "How long will the workout take in minutes?\n";
-	getline (cin,answer);
-	myfile << "duration: " << answer <<"\n";	
+	double duration_total;
+	cin >> duration_total;
+	myfile << "duration: " << duration_total <<"\n";	
 	
-	cout << "What is the type of the workout?\n";
-	getline (cin,answer);
+	cout << "What is the type of the workout?\n"; cin >> answer;
 	myfile << "type: " << answer <<"\n";	
 	
-	cout << "What is the max heartrate you can reach?\n";
-	getline (cin,answer);
+	cout << "What is the max heartrate you can reach?\n"; 
+	cin >> answer;
 	myfile << "maxHeartrate: " << answer <<"\n";
 	
 	cout << "What is the min heartrate you can reach?\n";
-	getline (cin,answer);
+	cin >> answer;
 	myfile << "minHeartrate: " << answer <<"\n";
+	
+	double duration_warmup;
+	bool timetyped = false;
+	while (timetyped = false){
+	cout << "How long does the warming up take, needs to be shorter then the total time\n";
+	cin >> duration_warmup;
+	if(duration_warmup<duration_total) {
+		timetyped = true;
+	}
+	}
+	myfile << "warmup_duration: " << duration_warmup <<"\n";
+	
+	
 	
 	int number_of_songs=0;
 	string song_answer;
@@ -42,19 +56,16 @@ while (answer_create_workout == "yes"){
 	cout << "Type the number of the song and press enter, type no for no more songs...\n";
 	myfile << "songs: ";
 	while (song_answer != "no"){
-	getline (cin,song_answer);
+	cin >> song_answer;
 	if (song_answer != "no"){
 		myfile << song_answer << ", ";
 		number_of_songs++;
 	}
 	}
 	cout << "\nyou have added " << number_of_songs << " songs.\n";
-	
-	
-	
-	
+		
 	cout << "Would you like to create a new program? \n";
-	getline (cin,answer_create_workout);
+	cin >> answer_create_workout;
 }
 
   return 0;
