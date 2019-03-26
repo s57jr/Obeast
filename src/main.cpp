cdedd97f3fcd9915a08546a8b67e91e496fbaa31
group E <jost.reberc@gmail.com>
2019-03-26 09:43:30 +0000

    compile isntruction added

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 #include "basicwarmup.h"
 
+/*
+ compile using:
+
+ g++ -Iheaders/ -o wazzaaappp src/main.cpp src/heartrate.cpp src/pressure.cpp src/sensor.cpp src/exercisebase.cpp
+ src/uart.cpp src/dsp.cpp src/bluetooth.cpp src/basicwarmup.cpp
+*/
+
 using namespace std;
 
 int main()
